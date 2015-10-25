f2975020f2d34ab2b6060d1917013b0c28fbdae4
Julian Fuchs <jfuchs@mit.edu>
2015-10-25 18:59:55 -0400

    Separate library and example code

---
+++ examples/fractal.rs
@@ -0,0 +1,39 @@
+extern crate turtle;
+
+use std::collections::HashMap;
+use turtle::{Turtle, turtle_draw_sdl};
+
+fn main() {
+    let mut rules = HashMap::new();
+    rules.insert('A', "+B-A-B+");
+    rules.insert('B', "-A+B+A-");
+    let mut state = "A".to_string();
+    for _ in 0..8 {
+        state = l_system_step(state, &rules);
+    }
+
+    let mut t = Turtle::new(600, 600);
+    t.move_to(40, 500);
+    t.set_color(255, 255, 255);
+    for c in state.chars() {
+        match c {
+            'A' | 'B' => { t.forward(2); },
+            '+' => { t.turn(-60.0); },
+            '-' => { t.turn(60.0); },
+            _ => {}
+        }
+    }
+    turtle_draw_sdl(&t);
+}
+
+fn l_system_step(state: String, rules: &HashMap<char, &str>) -> String {
+    let mut result = String::new();
+    for c in state.chars() {
+        if let Some(s) = rules.get(&c) {
+            result.push_str(s);
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-extern crate rand;
 extern crate sdl2;
-
-use std::collections::HashMap;
-
-use rand::Rng;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -21,7 +16,7 @@ fn d2r(r: f32) -> f32 {
     r * std::f32::consts::PI / 180.0
 }
 
-struct Turtle {
+pub struct Turtle {
     x: f32,
     y: f32,
     h: f32,
@@ -31,7 +26,7 @@ struct Turtle {
 }
 
 impl Turtle {
-    fn new(width: u32, height: u32) -> Turtle {
+    pub fn new(width: u32, height: u32) -> Turtle {
         let cx = width as f32 / 2.0;
         let cy = height as f32 / 2.0;
 
@@ -45,41 +40,41 @@ impl Turtle {
         }
     }
 
-    fn forward(&mut self, dist: i32) {
+    pub fn forward(&mut self, dist: i32) {
         self.x += (dist as f32) * f32::cos(d2r(self.h));
         self.y += (dist as f32) * f32::sin(d2r(self.h));
         self.ops.push(TurtleOps::LineTo(self.x, self.y));
     }
 
-    fn turn(&mut self, amt: f32) {
+    pub fn turn(&mut self, amt: f32) {
         self.h += amt;
     }
 
-    fn move_to(&mut self, nx: i32, ny: i32) {
+    pub fn move_to(&mut self, nx: i32, ny: i32) {
         self.x = nx as f32;
         self.y = ny as f32;
         self.ops.push(TurtleOps::MoveTo(self.x, self.y));
     }
 
-    fn set_color(&mut self, r: u8, g: u8, b: u8) {
+    pub fn set_color(&mut self, r: u8, g: u8, b: u8) {
         self.color = (r, g, b);
         self.ops.push(TurtleOps::SetColor(r, g, b));
     }
 
-    fn get_position(&self) -> (f32, f32) {
+    pub fn get_position(&self) -> (f32, f32) {
         (self.x, self.y)
     }
 
-    fn get_heading(&self) -> f32 {
+    pub fn get_heading(&self) -> f32 {
         self.h
     }
 
-    fn get_color(&self) -> (u8, u8, u8) {
+    pub fn get_color(&self) -> (u8, u8, u8) {
         self.color
     }
 }
 
-fn turtle_draw_sdl(turtle: &Turtle) {
+pub fn turtle_draw_sdl(turtle: &Turtle) {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem.window("rs-turtle", turtle.wdim.0, turtle.wdim.1)
@@ -144,48 +139,3 @@ fn turtle_draw_sdl(turtle: &Turtle) {
         std::thread::sleep_ms(delay);
     }
 }
-
-fn main() {
-    let mut rules = HashMap::new();
-    rules.insert('A', "+B-A-B+");
-    rules.insert('B', "-A+B+A-");
-    let mut state = "A".to_string();
-    for _ in 0..8 {
-        state = l_system_step(state, &rules);
-    }
-    println!("{}", state.len());
-
-    let mut rng = rand::thread_rng();
-
-    let mut t = Turtle::new(600, 600);
-
-    t.move_to(40, 500);
-    t.set_color(rng.gen(), rng.gen(), rng.gen());
-    for c in state.chars() {
-        match c {
-            'A' | 'B' => { t.forward(2); },
-            '+' => { t.turn(-60.0); },
-            '-' => { t.turn(60.0); },
-            _ => {}
-        }
-
-        if rng.gen::<f32>() < 0.005 {
-            t.set_color(rng.gen(), rng.gen(), rng.gen());
-        }
-    }
-    turtle_draw_sdl(&t);
-}
-
-
-fn l_system_step(state: String, rules: &HashMap<char, &str>) -> String {
-    let mut result = String::new();
-    for c in state.chars() {
-        if let Some(s) = rules.get(&c) {
-            result.push_str(s);
-        } else {
-            result.push(c);
-        }
-    }
-    result
-}
-
